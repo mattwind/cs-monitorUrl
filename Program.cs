c3d7e17d64e9d998a47d053816c983715507257a
Matt Wind <mattwind@gmail.com>
2017-04-17 11:43:50 -0700

    updated

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplication
 {
@@ -9,14 +12,33 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             List<string> list = new List<string>();
-            using (var fs = new FileStream("skus.txt", FileMode.Open, FileAccess.Read))
+            using (var fs = new FileStream("urls.txt", FileMode.Open, FileAccess.Read))
             using (var sr = new System.IO.StreamReader(fs))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    list.Add(line); 
-                    Console.WriteLine(line);
+                    // Add sku to list
+                    list.Add(line);
+                    // Download task                    
+                    ReadPage(line);             
+                    Thread.Sleep(5000); 
+                }
+            }
+        }
+        static async void ReadPage(string url)
+        {
+            var r = await DownloadPage(url);            
+            Console.WriteLine(r.Substring(0, 50));
+        }
+        static async Task<string> DownloadPage(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var r = await client.GetAsync(new Uri(url)))
+                {
+                    string result = await r.Content.ReadAsStringAsync();
+                    return result;
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # Monitor URL
 
 The purpose of the utility is to monitor a url for changes.
-
-## Base URL
-
-http://www.bestbuy.com/site/product/{SKU}.p?skuId={SKU}
-
-## SKUS
-
-* 5670003 - Nintendo Switch
-* 5723304 - Mario Kart 8
