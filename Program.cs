66e41c301d6aeceec1d5f467b03a20e4183304bd
Matt Wind <mattwind@gmail.com>
2017-04-17 11:24:12 -0700

    Derp

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            using (var fs = new FileStream("skus.txt", FileMode.Open, FileAccess.Read))
+            using (var sr = new System.IO.StreamReader(fs))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list.Add(line); 
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# cs-monitorUrl
+# Monitor URL
+
+The purpose of the utility is to monitor a url for changes.
+
+## Base URL
+
+http://www.bestbuy.com/site/product/{SKU}.p?skuId={SKU}
+
+## SKUS
+
+Nintendo Switch   5670003
+Mario Kart 8      5723304
